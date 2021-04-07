4607537700bfc3bf468b4c95a632abe4f84605b5
ngdnghia28 <ngd.nghia28@gmail.com>
2021-04-08 05:46:11 +0700

    port can be read from env

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const fs = require("fs");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const mbtilesPath = process.env.MBTILES_PATH;
 let mbtiles;
 
